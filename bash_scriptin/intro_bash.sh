a1b092347684a25d8f8a39b544b8ebfb5e0e95fe
RobHacks-1337 <142835112+RobHacks-1337@users.noreply.github.com>
2026-03-08 01:26:35 +0545

    Some scripts

--- bash_scriptin/intro_bash.sh
+++ bash_scriptin/intro_bash.sh
@@ -1,3 +1,12 @@
 #!/bin/bash	-- shebang
 
+echo Ai chan is kawaii 	# echo simply prints output in terminal
+echo "$PWD" 
+echo "$PATH" | tr : '\n'
+
+name='robin  bro  '
+foo=$(cowsay Robin is God)
+
+echo $name
+echo "$foo"  # The " " preserves the spaces and /n
 
